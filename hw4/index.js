3f304423ff5d603a1a282326040fefd8c4456bee
madeline.cwu@gmail.com <maddiewu>
2019-04-15 19:01:47 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -128,10 +128,10 @@ class Classifier {
     const inputBuffer = tf.buffer([1, this.maxLen], 'float32');
     for (let i = 0; i < inputText.length; ++i) {
       const word = inputText[i];
-      if (!isNaN(this.wordIndex[word])){
-        inputBuffer.set(this.wordIndex[word], 0, i); 
+      if (this.wordIndex[word]){
+        inputBuffer.set(this.wordIndex[word], 0, i);
         console.log(word, this.wordIndex[word], inputBuffer);
-      }
+      }  
     }
     const input = inputBuffer.toTensor();
     console.log(input);
