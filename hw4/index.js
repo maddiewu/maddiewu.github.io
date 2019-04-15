ccf7c2262302f8f9203e69932de4c183b4a83c0d
madeline.cwu@gmail.com <maddiewu>
2019-04-15 02:11:44 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -132,7 +132,7 @@ class Classifier {
       console.log(word, this.wordIndex[word], inputBuffer);
     }
     const input = inputBuffer.toTensor();
-    const console.log(input);
+    console.log(input);
     
     status('Running inference');
     const beginMs = performance.now();
